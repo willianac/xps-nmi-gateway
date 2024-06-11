dd9fbc2136bc1b351392a35d90774a75ad329826
Willian Almeida <willianac01@gmail.com>
2024-06-11 19:00:16 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import express from 'express';
+import cors from "cors";
 
 const app = express();
+
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || 'localhost';
