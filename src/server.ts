c1df4d5bb2d3b37a369cb93c9856f07776fec7c1
Willian Almeida <willianac01@gmail.com>
2024-06-11 18:37:11 -0300

    refactor: changes for tests purposes

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,16 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000
-const HOST = process.env.HOST ?? 'localhost';
+const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || 'localhost';
 
 app.get('/', (req, res) => {
+  console.log("received request at root", )
   res.send(`Hello from the server runnin on port ${PORT} and host ${HOST}`)
 });
 
 app.get("/test", (req, res) => {
+  console.log("received request at '/test'", )
   res.send("Respondendo com sucesso")
 })
 
